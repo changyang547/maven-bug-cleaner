104338643a22d35890712fe98ffa9172b710ea5e
GitHub Action <action@github.com>
2026-09-15 06:38:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/changyang547/mavenbugcleaner/MyBundle.kt
+++ src/main/kotlin/com/github/changyang547/mavenbugcleaner/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.changyang547.mavenbugcleaner
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/changyang547/mavenbugcleaner/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/changyang547/mavenbugcleaner/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.changyang547.mavenbugcleaner.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
